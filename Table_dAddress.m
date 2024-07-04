d134d268e2f29e0f34b520b943b4be27f21ca978
Gabriel Tedesque <121674757+GabrielTedesque@users.noreply.github.com>
2024-07-03 22:53:30 -0300

    Create Table_dAddress.m

---
+++ Table_dAddress.m
@@ -0,0 +1,39 @@
+let
+    // Definir a função para fazer a solicitação à API e obter os dados dos usuários
+    get_users = (seed) =>
+        let
+            // Fazer a solicitação à API e obter os dados dos usuários
+            response = Web.Contents("https://random-data-api.com/api/users/random_user?size=25&random_seed=" & Text.From(seed)),
+            json = Json.Document(response)
+        in
+            json,
+
+    // Utilizar List.Generate para repetir a solicitação até obter no máximo 150 registros
+    users = List.Generate(
+        // Função de inicialização
+        () => get_users(1),
+        // Condição de continuação
+        each List.Count(_) < 150,
+        // Função para obter o próximo valor
+        each let
+                seed = _{0}[id] + 1, // Incrementa o seed a partir do ID do primeiro usuário do conjunto
+                new_users = get_users(seed),
+                remaining_capacity = 150 - List.Count(_)
+             in
+                List.FirstN(new_users, remaining_capacity)
+    ),
+
+    // Converter a lista de listas em uma única lista de usuários
+    all_users = List.Combine(users),
+    #"Convertido para Tabela" = Table.FromList(all_users, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    #"Column1 Expandido" = Table.ExpandRecordColumn(#"Convertido para Tabela", "Column1", {"id", "uid", "password", "first_name", "last_name", "username", "email", "avatar", "gender", "phone_number", "social_insurance_number", "date_of_birth", "employment", "address", "credit_card", "subscription"}, {"id", "uid", "password", "first_name", "last_name", "username", "email", "avatar", "gender", "phone_number", "social_insurance_number", "date_of_birth", "employment", "address", "credit_card", "subscription"}),
+    #"Limitado a 150 Linhas" = Table.FirstN(#"Column1 Expandido", 150),
+    #"Duplicatas Removidas" = Table.Distinct(#"Limitado a 150 Linhas", {"id"}),
+    #"Colunas Removidas" = Table.RemoveColumns(#"Duplicatas Removidas",{"uid", "password", "first_name", "last_name", "username", "email", "avatar", "gender", "phone_number", "social_insurance_number", "date_of_birth", "employment", "credit_card", "subscription"}),
+    #"address Expandido" = Table.ExpandRecordColumn(#"Colunas Removidas", "address", {"city", "street_name", "street_address", "zip_code", "state", "country", "coordinates"}, {"city", "street_name", "street_address", "address.zip_code", "state", "country", "coordinates"}),
+    #"coordinates Expandido" = Table.ExpandRecordColumn(#"address Expandido", "coordinates", {"lat", "lng"}, {"lat", "lng"}),
+    #"Tipo Alterado" = Table.TransformColumnTypes(#"coordinates Expandido",{{"id", Int64.Type}}),
+    #"Linhas em Branco Removidas" = Table.SelectRows(#"Tipo Alterado", each not List.IsEmpty(List.RemoveMatchingItems(Record.FieldValues(_), {"", null}))),
+    #"Linhas em Branco Removidas1" = Table.SelectRows(#"Linhas em Branco Removidas", each not List.IsEmpty(List.RemoveMatchingItems(Record.FieldValues(_), {"", null})))
+in
+    #"Linhas em Branco Removidas1"
